84d750c928ed3d1b434f9b4a7ad3808627d5eb25
unknown <ines.benchouche@gmail.com>
2024-01-14 20:19:22 +0100

    insertion et suppression

--- cree.c
+++ cree.c
@@ -123,12 +123,128 @@ void VisualizationDuTri() {
     }
 
     CloseWindow();
+
 }
 
 
+void insertion(int tableau[], int *n, int valeur, int position) {
+    // Vérifier si la position est valide
+    if (position < 0 || position > *n) {
+        printf("Position invalide. L'insertion n'a pas été effectuée.\n");
+        return;
+    }
+
+    // Décaler les éléments pour faire de la place pour la nouvelle valeur
+    for (int i = *n - 1; i >= position; i--) {
+        tableau[i + 1] = tableau[i];
+    }
+
+    // Insérer la nouvelle valeur à la position spécifiée
+    tableau[position] = valeur;
+
+    // Incrémenter la taille du tableau
+    (*n)++;
+}
+
+void insertionAnimation(int tableau[], int *n, int valeur, int position) {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Insertion Animation with Raylib");
+
+    // Délai en millisecondes pour chaque étape de l'animation
+    int delay = 5000;
+
+    int i;
+
+    // Dessiner le tableau initial
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    for (i = 0; i < *n; ++i) {
+        DrawRectangle(i * 60 + 10, screenHeight / 2 - 20, 50, 40, RED);
+        DrawText(TextFormat("%d", tableau[i]), i * 60 + 30, screenHeight / 2 - 10, 20, BLACK);
+    }
+    EndDrawing();
+
+    // Attendre avant de commencer l'animation
+    sleep(delay);
+
+    // Délai pour déplacer les éléments vers la droite avant l'insertion
+    for (i = *n - 1; i >= position; i--) {
+        // ... (partie inchangée)
+    }
+
+    // Insérer la nouvelle valeur
+    tableau[position] = valeur;
+    (*n)++;
+
+    // Attendre que l'utilisateur appuie sur Entrée pour fermer la fenêtre
+    while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_ENTER)) {
+            break;
+        }
+    }
+
+    CloseWindow();
+}
+
+void suppression(int tableau[], int *n, int val) {
+    int i, j;
+    for (i = 0; i < *n; i++) {
+        if (tableau[i] == val) {
+            for (j = i; j < *n - 1; j++) {
+                tableau[j] = tableau[j + 1];
+            }
+
+            (*n)--;
+            break;
+        }
+    }
+}
+
+void visualisationsuppression(int tableau[], int *n, int val) {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Suppression Animation with Raylib");
+
+    int j, k;
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    for (k = 0; k < *n; ++k) {
+        DrawRectangle(k * 60 + 10, screenHeight / 2 - 20, 50, 40, RED);
+        DrawText(TextFormat("%d", tableau[k]), k * 60 + 30, screenHeight / 2 - 10, 20, BLACK);
+    }
+
+    DrawText(TextFormat("Valeur à supprimer : %d", val), 10, 10, 20, BLACK);
+    suppression(tableau, n, val);
+
+    for (k = 0; k < *n; ++k) {
+        DrawRectangle(k * 60 + 10, screenHeight / 2 - 20, 50, 40, RED);
+        DrawText(TextFormat("%d", tableau[k]), k * 60 + 30, screenHeight / 2 - 10, 20, BLACK);
+    }
+
+    DrawRectangle(j * 60 + 10, screenHeight / 2 - 20, 50, 40, GREEN);
+    DrawText("Suppression effectuée", 10, 40, 20, BLACK);
+
+    EndDrawing();
+    sleep(1000 * 5);
+
+    while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_ENTER)) {
+            break;
+        }
+    }
+
+    CloseWindow();
+}
 int main() {
     int tableau[MAX_SIZE];
     int taille;
+    int valeur=7;
+    int position=4;
 
     // Appeler la fonction pour remplir le tableau
     remplirTableauAleatoire(tableau,&taille);
@@ -136,5 +252,8 @@ int main() {
     // Appeler la fonction pour afficher le tableau
     //afficherTableau(tableau, taille);
     VisualizationDuTri();
+    insertionAnimation(tableau,&taille,valeur,position);
+    visualisationsuppression(tableau,&taille,valeur);
+    
     return 0;
 }
